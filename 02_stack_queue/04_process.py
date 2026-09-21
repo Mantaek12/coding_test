97dba08ffbc43bbebf42b2a4e41a82fd15c3e615
mantaek12 <minkih1234@kookmin.ac.kr>
2026-09-21 12:34:16 +0900

    Solve: 프로세스 정럴 문제

--- 02_stack_queue/04_process.py
+++ 02_stack_queue/04_process.py
@@ -1,3 +1,43 @@
 def solution(priorities, location):
     answer = 0
-    return answer
+
+    queue = []
+
+    for i, p in enumerate(priorities):
+        queue.append((i,p))
+
+    # print(f"init [list] : {queue}")
+
+    while queue:
+        current = queue.pop(0)
+        # print(f"line11: {queue}")
+
+        has_higher = False
+
+        for item in queue:
+            # print(current[1])
+            if current[1] < item[1]:
+                # queue.append[current]
+                # print(queue)
+                has_higher = True
+                break
+
+        if has_higher:
+            queue.append(current)
+            # print(queue)
+        else:
+            answer +=1
+            if current[0] == location:
+                return answer
+
+
+
+
+if __name__ == "__main__":
+    # priorities = [2, 1, 3, 2]
+    # location = 2
+    # print(solution(priorities, location))  
+
+    priorities2 = [1, 1, 9, 1, 1, 1]
+    location2 = 0
+    print(solution(priorities2, location2)) 

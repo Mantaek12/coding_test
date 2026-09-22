c6481ae3980099d65c1410e6880c65477b3c156f
mantaek12 <minkih1234@kookmin.ac.kr>
2026-09-22 19:24:49 +0900

    3-1. 반복문의 정의와 for문의 형태

--- cpp_lecture/lecture_3/01_for.cpp
+++ cpp_lecture/lecture_3/01_for.cpp
@@ -9,9 +9,33 @@ int main()
 
      for(int i = 0; i < 5; i++){
         cout << i << "번째입니다."<< endl;
-        
      }
-
     
+    
+    int c = 10;
+    int d = 10;
+
+    cout << "c는" << c << ", d는" << d << endl;
+    cout << "c++는" << c++ << endl;
+    cout << "c++는" << c << endl;           // 10에서 11로 증가됨 
+    cout << "++d는" << ++d << endl;
+    cout << "c는" << c << ", d는" << d << endl;
+
+    for(int i = 5; i > 0; i--){     // i > 0 | 좌항이 크다면 true / 그렇지 않다면 False임. 즉, true일때만 for문이 돈다.
+        // cout << "감소연산자" << endl;
+        cout << i << "번째입니다."<< endl;
+    }
+
+
+    // 비교 연산자
+    /*
+    < : 좌항이 작다
+    <= : 작거나 같다
+    > : 크다
+    >= : 크거나 같다
+    == : 같다
+    != : 같지 않다
+    */
+
 
 }

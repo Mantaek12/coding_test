8dc3a5bd32044c5c3ad6a3334097723e0463825a
mantaek12 <minkih1234@kookmin.ac.kr>
2026-09-22 14:59:35 +0900

    1-3. 문자형 자료형과 bool 자료형

---
+++ cpp_lecture/lecture_1/03_char_bool.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+
+    // int a = 77;
+    // char b = a;
+    // char c = 'a';
+    // // char d = "a";       // null 문자(\0), char은 하나의 문자만을 받기에 큰따옴표 사용 불가, 큰따옴표는 null문자가 포함됨
+    // cout << b << endl;
+    // cout << c << endl;
+    // // cout << d << endl;
+
+    // char d[] {'a', 'b', 'c'};
+    // cout << d << endl;
+
+    bool a = 0;
+    bool b = 1;
+    bool c = 10;
+
+    cout << a << " " <<  b << " " << c << endl;
+
+    return 0;
+}

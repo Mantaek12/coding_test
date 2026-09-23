7c56c6a2222019dd7dd823a3f870cfaea7529497
mantaek12 <minkih1234@kookmin.ac.kr>
2026-09-23 10:29:52 +0900

    4-1. if구문과 if else 구문

---
+++ cpp_lecture/lecture_4/01_if_else.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    if (false){
+        cout << "조건이";
+        cout << "참입니다. " << endl;
+    }
+
+    else if (true)
+        {
+            cout << "2 \n";
+        }
+
+    else
+        {
+            cout << "1 \n ";
+        }
+    
+
+    return 0;
+
+}
